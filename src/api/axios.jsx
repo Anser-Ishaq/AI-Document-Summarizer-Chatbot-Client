b5a19f4c7df4e9fb59d2c3c6a8988896833023be
Anser-Ishaq <anserishaq472@gmail.com>
2025-05-26 01:02:16 +0500

    fixed axios interceptors to prevent from timeout issue

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -5,7 +5,7 @@
   // Create Axios instance
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 10000,
+    timeout: 60000,
     headers: {
       "Content-Type": "application/json",
     },
